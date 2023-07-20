68f64f8d8e539e3007247b96fa732c61acaad44d
Altair <89061806+Altair-Farooque21@users.noreply.github.com>
2023-07-20 19:18:45 +0530

    sample mock data

---
+++ client/src/MockData.js
@@ -0,0 +1,186 @@
+export const mock = {
+     Home :{
+        PopularRestaurants :{
+            type: 0, // '0' means render as restuartrant card
+            title : "Popular Restaurants",
+            restaurants :[{
+                    name : "Domino's Pizza",
+                    rating : "4.5",
+                    catLog : "Pizzas , Maccorinis , Spagetti , French fries",
+                    cost : "$12",
+                    deliveryTime : "35"
+                },
+                {
+                    name : "Haveli Royal",
+                    rating : "4.3",
+                    catLog : "Biryanis, starters, deserts, beverages",
+                    cost : "$8",
+                    deliveryTime : "25"
+                },
+                {
+                    name : "Barqas Restaurants",
+                    rating : "4.0",
+                    catLog : "Mandis, Biryanis, Deserts, Beverages",
+                    cost : "$10",
+                    deliveryTime : "30"
+                },
+                {
+                    name : "Arabian Nights",
+                    rating : "4.1",
+                    catLog : "Mandis, Biryanis, Deserts, Beverages",
+                    cost : "$15",
+                    deliveryTime : "40"
+                },
+               {
+                    name : "QuickSand",
+                    rating : "4.7",
+                    catLog : "Milkshakes, Juices, Deserts, Ice-creams",
+                    cost : "$5",
+                    deliveryTime : "20"
+                },
+                {
+                    name : "Domino's Pizza",
+                    rating : "4.5",
+                    catLog : "Pizzas , Maccorinis , Spagetti , French fries",
+                    cost : "$12",
+                    deliveryTime : "35"
+                },
+                {
+                    name : "Haveli Royal",
+                    rating : "4.3",
+                    catLog : "Biryanis, starters, deserts, beverages",
+                    cost : "$8",
+                    deliveryTime : "25"
+                },
+                {
+                    name : "Barqas Restaurants",
+                    rating : "4.0",
+                    catLog : "Mandis, Biryanis, Deserts, Beverages",
+                    cost : "$10",
+                    deliveryTime : "30"
+                },
+                {
+                    name : "Arabian Nights",
+                    rating : "4.1",
+                    catLog : "Mandis, Biryanis, Deserts, Beverages",
+                    cost : "$15",
+                    deliveryTime : "40"
+                },
+                {
+                    name : "QuickSand",
+                    rating : "4.7",
+                    catLog : "Milkshakes, Juices, Deserts, Ice-creams",
+                    cost : "$5",
+                    deliveryTime : "20"
+                }
+            ]
+        },
+        TrendingItems :{
+            type : 1, // '1' means render as item card
+            title : "Trending Items",
+            dishes : [
+                {
+                    "Name": "Chicken Biryani",
+                    "Description": "Tasty chicken & rice cooked in a blend of aromatic spices",
+                    "Price": 12.99,
+                    "Rating": 4.1,
+                    "Category": "non-veg"
+                },
+                {
+                    "Name": "Beef Steak",
+                    "Description": "Juicy cut of beef grilled to your preference, served with seasoned veggies",
+                    "Price": 18.99,
+                    "Rating": 4.7,
+                    "Category": "non-veg"
+                },
+                {
+                    "Name": "Veggie Pizza",
+                    "Description": "Delicious pizza topped with a variety of fresh vegetables",
+                    "Price": 9.99,
+                    "Rating": 3.8,
+                    "Category": "veg"
+                },
+                {
+                    "Name": "Shrimp Pasta",
+                    "Description": "Flavorful pasta cooked with shrimp and a creamy garlic sauce",
+                    "Price": 15.99,
+                    "Rating": 4.3,
+                    "Category": "non-veg"
+                },
+                {
+                    "Name": "Fish Tacos",
+                    "Description": "Soft tortillas filled with grilled fish, salsa, and a creamy sauce",
+                    "Price": 10.99,
+                    "Rating": 4.0,
+                    "Category": "non-veg"
+                },
+                {
+                    "Name": "Chicken Salad",
+                    "Description": "Fresh veggies tossed with grilled chicken, olive oil, and vinegar dressing",
+                    "Price": 7.99,
+                    "Rating": 3.6,
+                    "Category": "non-veg"
+                },
+                {
+                    "Name": "Tomato Soup",
+                    "Description": "Creamy soup made with ripe tomatoes and seasoned with herbs and spices",
+                    "Price": 5.99,
+                    "Rating": 4.4,
+                    "Category": "veg"
+                },
+                {
+                    "Name": "Vegetable Stir Fry",
+                    "Description": "A variety of veggies sautéed in a savory sauce and served with rice",
+                    "Price": 9.99,
+                    "Rating": 4.2,
+                    "Category": "veg"
+                },
+                {
+                    "Name": "Iced Tea",
+                    "Description": "Refreshing iced tea sweetened and served with a slice of lemon",
+                    "Price": 3.99,
+                    "Rating": 4.0,
+                    "Category": "beverage"
+                },
+                {
+                    "Name": "Lemonade",
+                    "Description": "Sweet and tangy lemonade, the perfect thirst quencher",
+                    "Price": 4.99,
+                    "Rating": 4.3,
+                    "Category": "beverage"
+                }
+            ]
+            
+        },
+        MFAQs : [
+            {
+                "Question": "How can I find my favorite restaurant on your platform?",
+                "Answer": "You can use the search bar at the top of our homepage to find your favorite restaurant. Simply type the name of the restaurant and click on the search icon."
+            },
+            {
+                "Question": "What are the payment options available?",
+                "Answer": "We accept all major credit cards, debit cards, and also provide options for net banking. In some areas, we also provide the Cash On Delivery (COD) option."
+            },
+            {
+                "Question": "How can I track my order?",
+                "Answer": "Once you have placed your order, you can track it in real-time by going to the 'My Orders' section and clicking on the 'Track Order' button."
+            },
+            {
+                "Question": "What should I do if my order is late?",
+                "Answer": "If your order is late, please contact our customer service team. We will contact the restaurant and courier to expedite your delivery."
+            },
+            {
+                "Question": "What if I received the wrong order?",
+                "Answer": "We apologize for the inconvenience. Please contact our customer service team immediately. We will arrange for the correct order to be delivered to you as soon as possible."
+            },
+            {
+                "Question": "Can I cancel my order?",
+                "Answer": "Yes, you can cancel your order within a certain time period. Please note that some charges may apply."
+            },
+            {
+                "Question": "Are there any discounts or promotions currently running?",
+                "Answer": "Yes, we regularly run promotions and offer discounts. Please check the 'Offers' section on our website or app for the latest deals."
+            }
+        ]
+     }
+}
