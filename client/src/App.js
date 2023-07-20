24de6ee62e602f3acf65a93dd4cf7db271caec15
Altair <89061806+Altair-Farooque21@users.noreply.github.com>
2023-07-20 19:18:36 +0530

    Added routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,26 @@
-import React from "react"
+import React from "react";
+import { BrowserRouter as Router , Routes, Route } from "react-router-dom";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+import Home from "./pages/Home";
+import Restaurant from "./pages/Restaurant";
 
 function App() {
   return (
-    <div>
-      This is the home page for Bitenow web app
-    </div>
+    <Router>
+      <Routes>
+        <Route path ="/" element={<Home/>} />
+        <Route path ="/signup" element={<SignUp />} />
+        <Route path ="/signin" element={<SignIn />} />
+        <Route path ="/restuarant" element={<Restaurant />} />
+        <Route path ="/cartOrdering" element={''} />
+        <Route path ="/payments" element={''} />
+        <Route path ="/trackOrder" element={''} />
+        <Route path ="/profile/*" element={''} />
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
+  
