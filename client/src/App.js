487a050d45b07fe25e67f9fa2caf4ef839dc9bc9
Altair <89061806+Altair-Farooque21@users.noreply.github.com>
2023-07-23 18:51:12 +0530

    updated routes

--- client/src/App.js
+++ client/src/App.js
@@ -4,18 +4,20 @@ import SignUp from "./pages/SignUp";
 import SignIn from "./pages/SignIn";
 import Home from "./pages/Home";
 import Restaurant from "./pages/Restaurant";
+import CardOrdering from "./pages/CardOrdering";
+import TrackOrder from "./pages/TrackOrder";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path ="/" element={<Home/>} />
+        <Route path ="/" element={<Home />} />
         <Route path ="/signup" element={<SignUp />} />
         <Route path ="/signin" element={<SignIn />} />
         <Route path ="/restuarant" element={<Restaurant />} />
-        <Route path ="/cartOrdering" element={''} />
+        <Route path ="/placingOrder" element={ <CardOrdering />} />
+        <Route path ="/trackOrder" element={<TrackOrder />} />
         <Route path ="/payments" element={''} />
-        <Route path ="/trackOrder" element={''} />
         <Route path ="/profile/*" element={''} />
       </Routes>
     </Router>
