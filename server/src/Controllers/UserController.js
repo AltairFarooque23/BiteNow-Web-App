ad196ff90f422cb8d1005eb91a491765e213bcd4
Altair <89061806+Altair-Farooque21@users.noreply.github.com>
2023-07-30 12:04:58 +0530

    updated modules

---
+++ server/src/Controllers/UserController.js
@@ -0,0 +1,84 @@
+const userModel = require("../Models/User");
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+
+const signUpWithEmail = async (req,res) =>{
+    // existing user check
+    const {username , email , password} = req.body;
+    try{
+        // checking existing user
+        const existinguser = await userModel.findOne({  username : username  });
+        if(existinguser){
+            return res.status(400).json({message : "Username Already exists",errorCode : "register.exists.username"})
+        }
+
+        const existingEmail = await userModel.findOne({  email : email  });
+
+        if(existingEmail){
+            return res.status(400).json({message : "Email Already exists",errorCode : "register.exists.email"})
+        }
+        // hashing password
+        const hashedPassword = await bcrypt.hash(password,10);
+        // user create
+        const result  = await userModel.create({
+            email : email,
+            password : hashedPassword,
+            username : username
+        });
+        // token generate
+        const token = jwt.sign({
+            email: result.email,
+            id: result._id
+        },
+        process.env.JWT_TOKEN_SECRET_KEY);
+
+        // sending response to client
+        res.status(201).json({user:result,token:token});
+
+    }
+    catch (error){
+        console.log(error);
+        res.status(500).json({message : "Something went wrong"})
+    }
+
+}
+
+const signInWithEmail = async (req,res) =>{
+    const {email , password} = req.body;
+
+    try{
+        const existinguser = await userModel.findOne({email:email});
+        if(!existinguser){
+            return res.status(404).json({message : "User not exists",errorCode : "user.doesnotexists"})
+        }
+        const matchPassword = await bcrypt.compare(password,existinguser.password);
+        
+        if(!matchPassword){
+            return res.status(400).json({message: "Invalid Credentials",errorCode : "user.incorrectPassword"})
+        }
+
+        const token = jwt.sign({
+            email: existinguser.email,
+            id: existinguser._id
+        },
+        process.env.JWT_TOKEN_SECRET_KEY,{ expiresIn: '12h' });
+
+        return res.status(200).json({
+            user: existinguser,
+            token : token
+        })
+    }
+    catch(error){
+        console.log(error);
+        res.status(500).json({message : "Something went wrong"})
+    }
+    
+}
+
+const signUpWithGoogle = async (res,req)=>{
+    
+}
+
+
+module.exports = {signUpWithEmail,signInWithEmail}
